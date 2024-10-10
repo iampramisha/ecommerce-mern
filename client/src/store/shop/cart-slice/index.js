e367c762c469855414863c4a57fd7cf888d820d4
Pramisha <pramisha1209@gmail.com>
2024-10-10 17:25:03 +0545

    Fix import.meta.env issue

--- client/src/store/shop/cart-slice/index.js
+++ client/src/store/shop/cart-slice/index.js
@@ -2,7 +2,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const BASE_URL = `${imp.meta.env.VITE_API_URL}/api/shop/carts`;
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/shop/carts`;
 
 // Async thunks for handling API calls
 
