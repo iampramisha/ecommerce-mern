6104fbd8259b22d3ac125b23626c2f0743b74311
Pramisha <pramisha1209@gmail.com>
2024-10-11 01:20:01 +0545

    cookie

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -4,13 +4,8 @@ const paypal = require('paypal-rest-sdk');
 
 paypal.configure({
     mode:"sandbox",
-<<<<<<< HEAD
     client_id: 'ASj_bzOZwcoiDLMi0wkv8xpBZqVmCIBPAR5jdG29HK3tE2G97w6ZtCtSAQfonaWkvJdyKRNcFW15NiG3',
     client_secret: 'EBi-eomG5mxRMfMDk2k027BYYl8QHMcIijOGvHkr4a0YZa17N0gaJnr0TdlnppR1G5XGlNWy-JPbhdtY'
-=======
-    client_id: process.env.CLIENT_ID,
-    client_secret:process.env.CLIENT_SECRET
->>>>>>> e367c762c469855414863c4a57fd7cf888d820d4
 });
 console.log("PayPal configured with client ID:", paypal.configuration.client_id);
 
